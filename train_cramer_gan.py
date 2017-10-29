91a9bc734c71f41682b031e0ee9478753d611958
mr4msm <nc.jky0@gmail.com>
2017-10-30 00:46:04 +0900

    refactor.

--- train_cramer_gan.py
+++ train_cramer_gan.py
@@ -5,18 +5,16 @@ import argparse
 import chainer
 import numpy as np
 import os
-from chainer import Variable, cuda, serializers, using_config
+from chainer import Variable, cuda, using_config
 from chainer import computational_graph as graph
 from chainer import functions as F
 from datetime import datetime as dt
 
 from batch_generator import ImageBatchGenerator
+from commons import ModelOptimizerSet
 from commons import load_module
-from commons import initialize_model, initialize_optimizer
+from commons import init_model, init_optimizer
 from commons import l2_norm
-
-SAVE_PARAMS_FORMAT = 'trained-params_{0}_update-{1:09d}.npz'
-SAVE_STATE_FORMAT = 'optimizer-state_{0}_update-{1:09d}.npz'
 
 
 def parse_arguments():
@@ -88,13 +86,15 @@ if __name__ == '__main__':
     update_cri_per_gen = getattr(config, 'UPDATE_CRI_PER_GEN', 5)
     gp_lambda = getattr(config, 'LAMBDA', 10)
 
-    model_gen = config.Generator()
-    optimizer_gen = config.OPTIMIZER_GEN
-    optimizer_gen.setup(model_gen)
-
     model_cri = config.Critic()
     optimizer_cri = config.OPTIMIZER_CRI
     optimizer_cri.setup(model_cri)
+    model_opt_set_cri = ModelOptimizerSet(model_cri, optimizer_cri)
+
+    model_gen = config.Generator()
+    optimizer_gen = config.OPTIMIZER_GEN
+    optimizer_gen.setup(model_gen)
+    model_opt_set_gen = ModelOptimizerSet(model_gen, optimizer_gen)
 
     # setup batch generator
     with open(args.dataset, 'r') as f:
@@ -115,17 +115,17 @@ if __name__ == '__main__':
     print('lambda for gradient penalty = {}'.format(gp_lambda))
 
     # save or load initial parameters for Critic
-    initialize_model(model_cri, args.param_cri,
-                     os.path.join(out_dir, SAVE_PARAMS_FORMAT.format('cri', optimizer_cri.t)))
-    # save or load initial parameters for Generator
-    initialize_model(model_gen, args.param_gen,
-                     os.path.join(out_dir, SAVE_PARAMS_FORMAT.format('gen', optimizer_gen.t)))
+    if not init_model(model_cri, param=args.param_cri):
+        model_opt_set_cri.save_model('cri', out_dir=out_dir)
     # save or load initial optimizer state for Critic
-    initialize_optimizer(optimizer_cri, args.state_cri,
-                         os.path.join(out_dir, SAVE_STATE_FORMAT.format('cri', optimizer_cri.t)))
+    if not init_optimizer(optimizer_cri, state=args.state_cri):
+        model_opt_set_cri.save_optimizer('cri', out_dir=out_dir)
+    # save or load initial parameters for Generator
+    if not init_model(model_gen, param=args.param_gen):
+        model_opt_set_gen.save_model('gen', out_dir=out_dir)
     # save or load initial optimizer state for Generator
-    initialize_optimizer(optimizer_gen, args.state_gen,
-                         os.path.join(out_dir, SAVE_STATE_FORMAT.format('gen', optimizer_gen.t)))
+    if not init_optimizer(optimizer_gen, state=args.state_gen):
+        model_opt_set_gen.save_optimizer('gen', out_dir=out_dir)
 
     # set current device and copy model to it
     xp = np
@@ -145,13 +145,13 @@ if __name__ == '__main__':
 
     initial_t = optimizer_gen.t
     sum_count = 0
-    sum_loss_g = 0.
-    sum_loss_c = 0.
+    sum_loss_cri = 0.
+    sum_loss_gen = 0.
 
     # training loop
     while optimizer_gen.t < update_max:
 
-        for _ in range(update_cri_per_gen):
+        for idx in range(update_cri_per_gen):
             x_t = next(batch_generator)
             if gpu_id >= 0:
                 x_t = cuda.to_gpu(x_t, device=gpu_id)
@@ -161,13 +161,17 @@ if __name__ == '__main__':
             z2 = xp.random.normal(loc=0., scale=1.,
                                   size=(batch_size, z_vec_dim)).astype('float32')
 
-            with using_config('enable_backprop', False):
+            if idx == (update_cri_per_gen - 1):
                 x_g1 = model_gen(z1)
                 x_g2 = model_gen(z2)
+            else:
+                with using_config('enable_backprop', False):
+                    x_g1 = model_gen(z1)
+                    x_g2 = model_gen(z2)
 
             h_t = model_cri(x_t)
-            h_g1 = model_cri(x_g1)
-            h_g2 = model_cri(x_g2)
+            h_g1 = model_cri(x_g1.data)
+            h_g2 = model_cri(x_g2.data)
 
             loss_s = F.sum(f_critic(h_t, h_g2)) - F.sum(f_critic(h_g1, h_g2))
 
@@ -178,106 +182,71 @@ if __name__ == '__main__':
                                       ).astype('float32')
             interpolates = Variable((1. - alpha) * x_t + alpha * x_g1.data)
             h_i = model_cri(interpolates)
+            # print(f_critic(h_i, h_g2))
             gradients = chainer.grad([f_critic(h_i, h_g2)],
                                      [interpolates],
                                      enable_double_backprop=True)[0]
+            print('gradients, ', gradients)
             slopes = l2_norm(gradients)
+            # print(slopes)
             gradient_penalty = gp_lambda * F.sum((slopes - 1.) * (slopes - 1.))
+            # print(gradient_penalty)
 
-            loss_c = gradient_penalty - loss_s
-            loss_c /= batch_size
+            # print(f_critic(h_i, h_g2))
+            # print(interpolates.shape)
+            # print(h_t.data, h_g1.data, h_g2.data, h_i.data)
+
+            loss_cri = gradient_penalty - loss_s
+            loss_cri /= batch_size
+            # print('loss_cri, ', loss_cri)
 
             # update Critic
             model_cri.cleargrads()
-            loss_c.backward()
+            loss_cri.backward()
+            for name, param in model_cri.namedparams():
+                print(name, param.grad)
             optimizer_cri.update()
 
-            sum_loss_c += float(loss_c.data) / update_cri_per_gen
-
-        x_t = next(batch_generator)
-        if gpu_id >= 0:
-            x_t = cuda.to_gpu(x_t, device=gpu_id)
-
-        z1 = xp.random.normal(loc=0., scale=1.,
-                              size=(batch_size, z_vec_dim)).astype('float32')
-        z2 = xp.random.normal(loc=0., scale=1.,
-                              size=(batch_size, z_vec_dim)).astype('float32')
-
-        x_g1 = model_gen(z1)
-        x_g2 = model_gen(z2)
+            sum_loss_cri += float(loss_cri.data) / update_cri_per_gen
 
         h_t = model_cri(x_t)
         h_g1 = model_cri(x_g1)
         h_g2 = model_cri(x_g2)
 
-        loss_g = F.sum(l2_norm(h_t - h_g1)) + \
+        loss_gen = F.sum(l2_norm(h_t - h_g1)) + \
             F.sum(l2_norm(h_t - h_g2)) - F.sum(l2_norm(h_g1 - h_g2))
-        loss_g /= batch_size
+        loss_gen /= batch_size
 
         # update Generator
         model_gen.cleargrads()
-        loss_g.backward()
+        loss_gen.backward()
         optimizer_gen.update()
 
-        sum_loss_g += float(loss_g.data)
+        sum_loss_gen += float(loss_gen.data)
         sum_count += 1
 
         # show losses
         print('{0}: update # {1:09d}: C loss = {2:6.4e}, G loss = {3:6.4e}'.format(
             str(dt.now()), optimizer_gen.t,
-            float(loss_c.data), float(loss_g.data)))
+            float(loss_cri.data), float(loss_gen.data)))
 
         # output computational graph, if needed
         if args.computational_graph and optimizer_gen.t == (initial_t + 1):
             with open('graph.dot', 'w') as o:
-                o.write(graph.build_computational_graph((loss_c, )).dump())
+                o.write(graph.build_computational_graph((loss_cri, loss_gen)).dump())
             print('graph generated')
 
         # show mean losses, save interim trained parameters and optimizer states
         if optimizer_gen.t % update_save_params == 0:
             print('{0}: mean of latest {1:06d} in {2:09d} updates : C loss = {3:7.5e}, G loss = {4:7.5e}'.format(
-                str(dt.now()), sum_count, optimizer_gen.t, sum_loss_c / sum_count, sum_loss_g / sum_count))
+                str(dt.now()), sum_count, optimizer_gen.t, sum_loss_cri / sum_count, sum_loss_gen / sum_count))
             sum_count = 0
-            sum_loss_g = 0.
-            sum_loss_c = 0.
+            sum_loss_cri = 0.
+            sum_loss_gen = 0.
 
-            output_file_path = os.path.join(
-                out_dir, SAVE_PARAMS_FORMAT.format('gen', optimizer_gen.t))
-            serializers.save_npz(output_file_path, model_gen)
-            print('save ' + output_file_path)
-
-            output_file_path = os.path.join(
-                out_dir, SAVE_PARAMS_FORMAT.format('cri', optimizer_cri.t))
-            serializers.save_npz(output_file_path, model_cri)
-            print('save ' + output_file_path)
-
-            output_file_path = os.path.join(
-                out_dir, SAVE_STATE_FORMAT.format('gen', optimizer_gen.t))
-            serializers.save_npz(output_file_path, optimizer_gen)
-            print('save ' + output_file_path)
-
-            output_file_path = os.path.join(
-                out_dir, SAVE_STATE_FORMAT.format('cri', optimizer_cri.t))
-            serializers.save_npz(output_file_path, optimizer_cri)
-            print('save ' + output_file_path)
+            model_opt_set_gen.save('gen', out_dir=out_dir)
+            model_opt_set_cri.save('cri', out_dir=out_dir)
 
     # save final trained parameters and optimizer states
-    output_file_path = os.path.join(
-        out_dir, SAVE_PARAMS_FORMAT.format('gen', optimizer_gen.t))
-    serializers.save_npz(output_file_path, model_gen)
-    print('save ' + output_file_path)
-
-    output_file_path = os.path.join(
-        out_dir, SAVE_PARAMS_FORMAT.format('cri', optimizer_cri.t))
-    serializers.save_npz(output_file_path, model_cri)
-    print('save ' + output_file_path)
-
-    output_file_path = os.path.join(
-        out_dir, SAVE_STATE_FORMAT.format('gen', optimizer_gen.t))
-    serializers.save_npz(output_file_path, optimizer_gen)
-    print('save ' + output_file_path)
-
-    output_file_path = os.path.join(
-        out_dir, SAVE_STATE_FORMAT.format('cri', optimizer_cri.t))
-    serializers.save_npz(output_file_path, optimizer_cri)
-    print('save ' + output_file_path)
+    model_opt_set_gen.save('gen', out_dir=out_dir)
+    model_opt_set_cri.save('cri', out_dir=out_dir)
